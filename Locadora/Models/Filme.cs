82c1b4cacbf68041ee4d8961bd0ee185b4fb9461
Guilherme Felipe <guilhermeeefelipe@hotmail.com>
2021-06-11 17:18:22 -0300

    Correção Models

--- Locadora/Models/Filme.cs
+++ Locadora/Models/Filme.cs
@@ -18,7 +18,6 @@ namespace Locadora.Models
         public DateTime dt_criacao { get; set; }
         public bool st_ativo { get; set; }
         public virtual List<Genero> Generos { get; set; }
-        [NotMapped]
-        public SelectListItem ids { get; set; }
+
     }
 }
---
+++ Locadora/Models/Locacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Locacao
+    {
+        [Key]
+        public int cd_locacao { get; set; }
+        public virtual List<Filme> ListaFilmes { get; set; }
+
+        public string cpf { get; set; }
+        public DateTime dt_locacao { get; set; }
+    }
+}
